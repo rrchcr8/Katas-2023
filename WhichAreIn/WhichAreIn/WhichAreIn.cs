9f0f3ed676f17fd1f540aa1c9c3a75c2d2f240f7
Carlos Richter <crichter@ecisolutions.com>
2023-03-31 09:50:25 -0400

    Best practice

--- WhichAreIn/WhichAreIn/WhichAreIn.cs
+++ WhichAreIn/WhichAreIn/WhichAreIn.cs
@@ -12,16 +12,10 @@ public class WhichAreIn
     //https://www.codewars.com/kata/550554fd08b86f84fe000a58/train/csharp
     public static string[] inArray(string[] array1, string[] array2)
     {
-        var result = new HashSet<string>();
-        var sorted = array1.OrderBy(x => x).ThenBy(x => x.Length);
-        foreach (var item in sorted)
-        {
-            foreach(var item2 in array2)
-            { 
-                if(item2.Contains(item)) result.Add(item);
-            }
-        }
-        return result.ToArray();
+        return array1.Distinct()
+                             .Where(s1 => array2.Any(s2 => s2.Contains(s1)))
+                             .OrderBy(s => s)
+                             .ToArray();
     }
 
 }
