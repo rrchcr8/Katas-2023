4eebd30dbd3c4f95a1a54dd634346e842d718198
Carlos Richter <crichter@ecisolutions.com>
2023-05-02 16:15:36 -0400

    leet code solution

---
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ValidParentheses;
+
+Console.WriteLine("Hello, World!");
+
+var sol = new Solution();
+Console.WriteLine(sol.IsValid("((()))"));
---
+++ ValidParentheses/ValidParentheses/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+   public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c is '(' or '{' or '[')
+                    stack.Push(c);
+                if (c is '}' or ')' or ']')
+                {
+                    if (stack.Count > 0)
+                    {
+                        var peek = stack.Pop();
+
+                        if (peek == '(' && c != ')'
+                            || peek == '{' && c != '}'
+                            || peek == '[' && c != ']')
+                            return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
